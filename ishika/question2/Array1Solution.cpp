17ab7c81dfe7c07c623d997d4e3a8b29a290b537
ishika <imusun24@gmail.com>
2020-04-29 22:29:17 +0530

    sorting odd,even,pos,neg number using array

--- ishika/question2/Array1Solution.cpp
+++ ishika/question2/Array1Solution.cpp
@@ -1,3 +1,12 @@
+/*Take 20 integer inputs from user and print the following:
+number of positive numbers
+number of negative numbers
+number of odd numbers
+number of even numbers
+number of 0.
+
+*/
+
 #include<iostream>
 using namespace std;
 
