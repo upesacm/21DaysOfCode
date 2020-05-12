32bcbb074423c8fcc5ffa57494ef23678d644f56
tejas-trivedi <500077185@stu.upes.ac.in>
2020-05-12 11:14:17 +0530

    binary search

--- Tejas/Binary Search/binary_search.cpp
+++ Tejas/Binary Search/binary_search.cpp
@@ -4,8 +4,22 @@
 
 using namespace std;
 
-int binary_search();
-
+int binary_search(vector<int> &a, int x)
+{
+  int left = 0;
+  int right = (int)a.size() - 1;
+  while (left <= right)
+  {
+    int mid = left + (right - left) / 2;
+    if (x == a[mid])
+      return mid;
+    if (a[mid] > x)
+      right = mid - 1;
+    else
+      left = mid + 1;
+  }
+  return -1;
+}
 
 int linear_search(const vector<int> &a, int x)
 {
@@ -36,22 +50,6 @@ int main()
   }
 }
 
-int binary_search(vector<int> &a, int x)
-{
-  int left = 0;
-  int right = (int)a.size() - 1;
-  while (left <= right)
-  {
-    int mid = left + (right - left) / 2;
-    if (x == a[mid])
-      return mid;
-    if (a[mid] > x)
-      right = mid - 1;
-    else
-      left = mid + 1;
-  }
-  //write your code here
-  return -1;
-}
+
 
 
