0097aac7c141952a87f54dc05ace21ada7f25a9f
kushagra67414 <46487696+kushagra67414@users.noreply.github.com>
2020-05-29 14:21:20 +0530

    comparator

---
+++ kushagra/day2/Solution.java
@@ -0,0 +1,46 @@
+
+import java.lang.*; 
+import java.io.*;
+// Write your Checker class here
+class Checker implements Comparator<Player> 
+{ 
+    // Used for sorting in ascending order of 
+    // roll name 
+    public int compare(Player a, Player b){
+        if (a.score == b.score){
+            return a.name.compareTo(b.name);//alphabetically
+        } else {
+            return b.score - a.score;//decreasing 
+        }
+    } 
+} 
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+     
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
